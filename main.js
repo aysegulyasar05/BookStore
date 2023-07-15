8081fbf60541f45cde0f4bde9a67ed439b0cdc7d
aysegulyasar05 <aysegulceran05@gmail.com>
2023-07-15 10:16:35 +0200

    first commit

--- main.js
+++ main.js
@@ -1,32 +1,50 @@
-let bookList = [] , basketList = [];
+let bookList = [], basketList = []; toastr.options = {
+    "closeButton": false,
+    "debug": false,
+    "newestOnTop": false,
+    "progressBar": false,
+    "positionClass": "toast-bottom-right",
+    "preventDuplicates": false,
+    "onclick": null,
+    "showDuration": "300",
+    "hideDuration": "1000",
+    "timeOut": "5000",
+    "extendedTimeOut": "1000",
+    "showEasing": "swing",
+    "hideEasing": "linear",
+    "showMethod": "fadeIn",
+    "hideMethod": "fadeOut"
+}
 
-const toggleModal =()=>{
+
+
+const toggleModal = () => {
     const basketModalEl = document.querySelector(".basket-modal");
     basketModalEl.classList.toggle("active")
 };
 
 //Kitabimizi cagiracagimiz bir method product.JSON
 
-const getBooks= ()=>{
+const getBooks = () => {
     fetch("/products.json")
-    .then(res => res.json())
-    .then((books) => (bookList = books));
+        .then(res => res.json())
+        .then((books) => (bookList = books));
 }
 getBooks();
-const createBookStars = (starRate)=>{
-    let starRateHtml= "";
-    for(let i=1; i<=5; i++){
+const createBookStars = (starRate) => {
+    let starRateHtml = "";
+    for (let i = 1; i <= 5; i++) {
         if (Math.round(starRate) >= i) starRateHtml += `<i class="bi bi-star-fill active"></i>`;
         else starRateHtml += `<i class="bi bi-star-fill"></i>`;
     }
     return starRateHtml
 };
-const createBookItemsHtml = () =>{
+const createBookItemsHtml = () => {
     const bookListEl = document.querySelector(".book-list");
     let bookListHtml = "";
 
-    bookList.forEach((book,index) => {
-        bookListHtml += ` <div class="col-5 ${index%2 == 0 && "offset-2"} my-5">
+    bookList.forEach((book, index) => {
+        bookListHtml += ` <div class="col-5 ${index % 2 == 0 && "offset-2"} my-5">
                 <div class="row book-card">
                     <div class="col-6">
                         <img class="img-fluid shadow" src="${book.imgSource}" width:300 height:300>
@@ -44,8 +62,8 @@ const createBookItemsHtml = () =>{
                         <p class="book-description fos gray">${book.description} </p>
                         <div>
                             <span class="black fw-bold fs-5">${book.price}€</span>
-                            ${book.oldPrice ? 
-                                `<span class="gray fw-bold text-decoration-line-through">${book.oldPrice}€</span>` : ''}
+                            ${book.oldPrice ?
+                `<span class="gray fw-bold text-decoration-line-through">${book.oldPrice}€</span>` : ''}
                         </div>
                         <button class="btn-purple" onclick="addBookToBasket(${book.id})" >ADD BASKET</button>
                     </div>
@@ -57,13 +75,13 @@ const createBookItemsHtml = () =>{
 }
 
 const bookTypes = {
-    ALL : 'Tümü',
-    NOVEL :'Roman',
-    CHILDREN : 'Cocuk',
-    SELFIMPROVEMENT :'Kisisel Gelisim',
-    HISTORY : 'Tarih',
-    FINANCE : 'Finans',
-    SCIENCE :'Bilim'
+    ALL: 'Tümü',
+    NOVEL: 'Roman',
+    CHILDREN: 'Cocuk',
+    SELFIMPROVEMENT: 'Kisisel Gelisim',
+    HISTORY: 'Tarih',
+    FINANCE: 'Finans',
+    SCIENCE: 'Bilim'
 }
 
 
@@ -72,14 +90,14 @@ const createBookTypesHtml = () => {
     let filterHtml = "";
     let filterTypes = ['ALL'];
     bookList.forEach(book => {
-        if(filterTypes.findIndex(filter => filter == book.type)== -1) filterTypes.push(book.type);
+        if (filterTypes.findIndex(filter => filter == book.type) == -1) filterTypes.push(book.type);
     });
 
-    filterTypes.forEach((type,index) => {
-    filterHtml += `<li class="${index == 0 ? "active" : null} " onclick= "filterBooks(this)" data-type= "${type}">${bookTypes[type] || type} </li>`;
+    filterTypes.forEach((type, index) => {
+        filterHtml += `<li class="${index == 0 ? "active" : null} " onclick= "filterBooks(this)" data-type= "${type}">${bookTypes[type] || type} </li>`;
     });
 
-    filterEl.innerHTML=filterHtml;
+    filterEl.innerHTML = filterHtml;
 };
 
 
@@ -100,7 +118,7 @@ const listBasketItems = () => {
           width="80"
           height="80"
         />
-        <div class="basket-tem-info">
+        <div class="basket-item-info">
           <h3 class="book-name">${item.product.name}</h3>
           <span class="book-price">${item.product.price}€</span><br />
           <span class="book-remove" onclick="removeItemToBasket(${item.product.id})">remove</span>
@@ -116,7 +134,7 @@ const listBasketItems = () => {
     basketListEl.innerHTML = basketListHtml ? basketListHtml : ` <li>
                     No Items to Buy again..
                 </li>`;
-    totalPriceEl.innerHTML = totalPrice>0 ? "Total : " +totalPrice.toFixed(2)+ "€" :null
+    totalPriceEl.innerHTML = totalPrice > 0 ? "Total : " + totalPrice.toFixed(2) + "€" : null
 };
 
 
@@ -133,54 +151,62 @@ const filterBooks = (filterEl) => {
 
 const addBookToBasket = (bookId) => {
     const findedBook = bookList.find(book => book.id == bookId);
-   if(findedBook){
-    const basketAlreadyIndex = basketList.findIndex((basket)=>basket.product.id == bookId);
-       if (basketAlreadyIndex == -1) {
-           let addedItem = { quantity: 1, product: findedBook };
-           basketList.push(addedItem);
-}else{
-    basketList[basketAlreadyIndex].quantity +=1;
-}
-   listBasketItems(); 
-   };
+    if (findedBook) {
+        const basketAlreadyIndex = basketList.findIndex((basket) => basket.product.id == bookId);
+        if (basketAlreadyIndex == -1) {
+            let addedItem = { quantity: 1, product: findedBook };
+            basketList.push(addedItem);
+        } else {
+            if (basketList[basketAlreadyIndex].quantity < basketList[basketAlreadyIndex].product.stock)
+                basketList[basketAlreadyIndex].quantity += 1;
+         else {
+                toastr.error("Sorry we dont have enough stock.!");
+                return;
+         }
+    }
+    listBasketItems();
+    toastr.success("Successfully added!");
+};
 };
 
 const removeItemToBasket = (bookId) => {
-const findedIndex = basketList.findIndex(
-    (basket) => basket.product.id == bookId);
-if(findedIndex != -1){
-    basketList.splice(findedIndex,1);
-};
-listBasketItems();
+    const findedIndex = basketList.findIndex(
+        (basket) => basket.product.id == bookId);
+    if (findedIndex != -1) {
+        basketList.splice(findedIndex, 1);
+    }
+    listBasketItems();
 };
 
 const decreaseItemToBasket = (bookId) => {
     const findedIndex = basketList.findIndex(
-        (basket) => basket.product.id == bookId);
-
-        if(findedIndex != -1){
-           if( basketList[findedIndex].quantity != 1)
-            basketList[findedIndex].quantity -=1;
-            else removeItemToBasket(bookId);
-        };
+        (basket) => basket.product.id == bookId
+    );
+    if (findedIndex != -1) {
+        if (basketList[findedIndex].quantity != 1)
+            basketList[findedIndex].quantity -= 1;
+        else removeItemToBasket(bookId);
         listBasketItems();
+    }
 };
 
 const increaseItemToBasket = (bookId) => {
     const findedIndex = basketList.findIndex(
-        (basket) => basket.product.id == bookId);
-
+        (basket) => basket.product.id == bookId
+    );
     if (findedIndex != -1) {
-        if (basketList[findedIndex].quantity == basketList[findedIndex].product.stock)
+        if (
+            basketList[findedIndex].quantity < basketList[findedIndex].product.stock
+        )
             basketList[findedIndex].quantity += 1;
-        else addItemToBasket(bookId);
-    };
-    listBasketItems();
+        else toastr.error("Sorry, we don't have enough stock.");
+        listBasketItems();
+    }
 };
-setTimeout(() =>{
+setTimeout(() => {
     createBookItemsHtml();
     createBookTypesHtml();
 }, 100);
 
-toastr.success('Başarılı!', 'İşlem Tamamlandı');
+
 
